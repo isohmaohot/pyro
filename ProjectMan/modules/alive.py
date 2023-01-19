80cc0cf2178a71476739e753a6bba67ae844d9c1
isohmaohot <sarahlaurel713@gmail.com>
2023-01-19 02:07:44 +0000

    update
    Signed-off-by: isohmaohot <sarahlaurel713@gmail.com>

--- ProjectMan/modules/alive.py
+++ ProjectMan/modules/alive.py
@@ -33,7 +33,7 @@ from .help import add_command_help
 
 modules = CMD_HELP
 alive_logo = (
-    gvarstatus("ALIVE_LOGO") or "https://telegra.ph/file/9dc4e335feaaf6a214818.jpg"
+    gvarstatus("ALIVE_LOGO") or "https://telegra.ph/Martin-nih-bos-01-17"
 )
 emoji = gvarstatus("ALIVE_EMOJI") or "⚡️"
 alive_text = gvarstatus("ALIVE_TEKS_CUSTOM") or "Hey, I am alive."
